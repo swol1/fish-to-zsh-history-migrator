6caeb236bfd01be500d21f84080e408ff70ed16c
swol1 <den.gavrilin1@gmail.com>
2023-10-29 05:41:30 +0100

    Add script for migrating history from Fish shell to Zsh

---
+++ Readme.md
@@ -0,0 +1,23 @@
+# Fish to Zsh History Migrator
+
+This is just a script to migrate fish shell history into zsh history.
+
+## Usage
+
+This script can be executed directly from the command line:
+
+Run the script using Ruby. You can optionally specify the paths to your Fish and Zsh history files.
+
+```shell
+cd fish-to-zsh-history-migrator
+ruby migrate.rb fish_history_path zsh_history_path
+```
+
+- `fish_history_path` (optional) - Full path to your fish history file. If not provided, the script will assume the default location (`~/.local/share/fish/fish_history`).
+- `zsh_history_path` (optional) - Full path to your zsh history file. If not provided, the script will assume the default location (`~/.zsh_history`).
+
+Example:
+
+```shell
+ruby migrate.rb ~/my_custom_path/fish_history ~/my_custom_path/zsh_history
+```
---
+++ migrate.rb
@@ -0,0 +1,57 @@
+require 'yaml'
+require 'fileutils'
+
+class FishToZshHistoryMigrator
+  def initialize(fish_history_path = nil, zsh_history_path = nil)
+    @fish_history_path = fish_history_path || File.join(user_home, '.local', 'share', 'fish', 'fish_history')
+    @zsh_history_path = zsh_history_path || File.join(user_home, '.zsh_history')
+
+    raise "Fish history file does not exist at #{@fish_history_path}" unless File.exist?(@fish_history_path)
+    raise "Zsh history file does not exist at #{@zsh_history_path}" unless File.exist?(@zsh_history_path)
+  end
+
+  def migrate
+    content = parse_fish_history.join("\n") + "\n"
+    File.write(@zsh_history_path, content, mode: 'a')
+    puts 'Migration completed successfully.'
+  rescue => e
+    puts "Error during migration: #{e.message}"
+    exit(1)
+  end
+
+  private
+
+  def user_home
+    ENV['HOME'] || ENV['USERPROFILE'] || (raise 'Home directory not found')
+  end
+
+  def parse_fish_history
+    entries = []
+    command_entry = {}
+    File.foreach(@fish_history_path) do |line|
+      case line.strip
+      when /- cmd: (.+)/
+        entries << format_for_zsh(command_entry) unless command_entry.empty?
+        command_entry = {'cmd' => $1.strip}
+      when /when: (.+)/
+        command_entry['when'] = $1.strip.to_i
+      end
+    end
+    entries << format_for_zsh(command_entry) unless command_entry.empty?
+    entries.compact
+  end
+
+  def format_for_zsh(entry)
+    command = entry['cmd']
+    timestamp = entry['when']
+
+    return unless command && timestamp
+
+    ": #{timestamp}:0;#{command.to_s.gsub(/\\\\/, '\\').gsub(/\\n/, "\n")}"
+  end
+end
+
+if __FILE__ == $0
+  fish_history_path, zsh_history_path = ARGV
+  FishToZshHistoryMigrator.new(fish_history_path, zsh_history_path).migrate
+end
